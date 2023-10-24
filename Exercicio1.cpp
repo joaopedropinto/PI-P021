6cab1faa796187623a900f21a06511df3ccfb3c3
joaopedropinto <jpcic07@gmail.com>
2023-10-23 21:33:41 -0300

    adicionando ajustes

--- Exercicio1.cpp
+++ Exercicio1.cpp
@@ -6,10 +6,11 @@
 using namespace std;
 
 // Classe para representar um Livro
-class Livro {
+class Livro
+{
 public:
     // Construtor da classe Livro
-    Livro(const string &titulo, const string &autor, int numCopias) 
+    Livro(const string &titulo, const string &autor, int numCopias)
         : titulo(titulo), autor(autor), numCopias(numCopias) {}
 
     // Métodos para obter informações do livro
@@ -18,8 +19,12 @@ public:
     int getNumCopias() const { return numCopias; }
 
     // Método para emprestar um livro, decrementando a quantidade disponível
-    void emprestar() { if(numCopias > 0) numCopias--; }
-    
+    void emprestar()
+    {
+        if (numCopias > 0)
+            numCopias--;
+    }
+
     // Método para devolver um livro, incrementando a quantidade disponível
     void devolver() { numCopias++; }
 
@@ -30,83 +35,95 @@ private:
 };
 
 // Classe para representar um Emprestimo de livro
-class Emprestimo {
+class Emprestimo
+{
 public:
     Emprestimo(Livro *livro, const string &data) : livro(livro), data(data) {}
 
-    Livro* getLivro() const { return livro; }
+    Livro *getLivro() const { return livro; }
     string getData() const { return data; }
 
 private:
-    Livro* livro;  // Ponteiro para o livro emprestado
-    string data;   // Data do Emprestimo
+    Livro *livro; // Ponteiro para o livro emprestado
+    string data;  // Data do Emprestimo
 };
 
 // Classe para representar um usuario
-class Usuario {
+class Usuario
+{
 public:
-    string nome;  // Nome do usuario, tornando público por simplicidade
+    string nome; // Nome do usuario, tornando público por simplicidade
 
     Usuario(const string &nome) : nome(nome) {}
 
     // Método para o usuario pegar um livro emprestado
-    void pegarLivro(Livro &livro) {
+    void pegarLivro(Livro &livro)
+    {
         livro.emprestar();
         time_t now = time(0);
         string data = ctime(&now);
-        data = data.substr(0, data.length()-1);  // Remover o '\n' do final
+        data = data.substr(0, data.length() - 1); // Remover o '\n' do final
         emprestimos.push_back(Emprestimo(&livro, data));
     }
 
     // Método para obter os livros emprestados pelo usuario
-    vector<Emprestimo> getEmprestimos() const {
+    vector<Emprestimo> getEmprestimos() const
+    {
         return emprestimos;
     }
 
 private:
-    vector<Emprestimo> emprestimos;  // Lista de livros emprestados pelo usuario
+    vector<Emprestimo> emprestimos; // Lista de livros emprestados pelo usuario
 };
 
-class Biblioteca {
+class Biblioteca
+{
 public:
-    static void adicionarLivro(const string &titulo, const string &autor, int numCopias) {
+    static void adicionarLivro(const string &titulo, const string &autor, int numCopias)
+    {
         livros.push_back(Livro(titulo, autor, numCopias));
     }
 
-    static bool verificarDisponibilidade(const string &titulo) {
-        for(const auto &livro : livros) {
-            if(livro.getTitulo() == titulo && livro.getNumCopias() > 0) {
+    static bool verificarDisponibilidade(const string &titulo)
+    {
+        for (const auto &livro : livros)
+        {
+            if (livro.getTitulo() == titulo && livro.getNumCopias() > 0)
+            {
                 return true;
             }
         }
         return false;
     }
 
-    static void listarLivrosEmprestados(const Usuario &usuario) {
+    static void listarLivrosEmprestados(const Usuario &usuario)
+    {
         auto emprestimos = usuario.getEmprestimos();
-        for(const auto &emprestimo : emprestimos) {
-            cout << emprestimo.getLivro()->getTitulo() << " - " << emprestimo.getLivro()->getAutor() 
+        for (const auto &emprestimo : emprestimos)
+        {
+            cout << emprestimo.getLivro()->getTitulo() << " - " << emprestimo.getLivro()->getAutor()
                  << " (Data do Emprestimo: " << emprestimo.getData() << ")" << endl;
         }
     }
 
-    static vector<Livro>& getLivros() { return livros; }
+    static vector<Livro> &getLivros() { return livros; }
 
 private:
-    static vector<Livro> livros;  
+    static vector<Livro> livros;
 };
 
 vector<Livro> Biblioteca::livros;
 
-
-int main() {
+int main()
+{
     int opcao;
     string titulo, autor;
     int numCopias;
-    Usuario* usuarioAtual = nullptr;  // Ponteiro para o usuario atual
+    Usuario *usuarioAtual = nullptr; // Ponteiro para o usuario atual
 
     // Loop do menu principal
-    while (true) {
+    while (true)
+    {
         cout << "\n--- Menu Biblioteca ---\n";
         cout << "1. Adicionar livro\n";
         cout << "2. Registrar usuario\n";
@@ -119,71 +136,84 @@ int main() {
         cin.ignore();
 
         // Opções do menu
-        switch (opcao) {
-            case 1:
-                // Adicionar livro à biblioteca
-                cout << "Título: ";
-                getline(cin, titulo);
-                cout << "Autor: ";
-                getline(cin, autor);
-                cout << "Número de cópias: ";
-                cin >> numCopias;
-                Biblioteca::adicionarLivro(titulo, autor, numCopias);
-                break;
-            
-            case 2:
-                // Registrar um novo usuario
-                cout << "Nome do usuario: ";
-                getline(cin, titulo);
-                usuarioAtual = new Usuario(titulo);
-                break;
+        switch (opcao)
+        {
+        case 1:
+            // Adicionar livro à biblioteca
+            cout << "Título: ";
+            getline(cin, titulo);
+            cout << "Autor: ";
+            getline(cin, autor);
+            cout << "Número de cópias: ";
+            cin >> numCopias;
+            Biblioteca::adicionarLivro(titulo, autor, numCopias);
+            break;
 
-            case 3:
-                // Pegar um livro emprestado
-                if (usuarioAtual) {
-                    cout << "Título do livro para emprestar: ";
-                    getline(cin, titulo);
-                    for (auto &livro : Biblioteca::getLivros()) {
-                        if (livro.getTitulo() == titulo && livro.getNumCopias() > 0) {
-                            usuarioAtual->pegarLivro(livro);
-                            cout << "Livro emprestado com sucesso!\n";
-                            break;
-                        }
+        case 2:
+            // Registrar um novo usuario
+            cout << "Nome do usuario: ";
+            getline(cin, titulo);
+            usuarioAtual = new Usuario(titulo);
+            break;
+
+        case 3:
+            // Pegar um livro emprestado
+            if (usuarioAtual)
+            {
+                cout << "Título do livro para emprestar: ";
+                getline(cin, titulo);
+                for (auto &livro : Biblioteca::getLivros())
+                {
+                    if (livro.getTitulo() == titulo && livro.getNumCopias() > 0)
+                    {
+                        usuarioAtual->pegarLivro(livro);
+                        cout << "Livro emprestado com sucesso!\n";
+                        break;
                     }
-                } else {
-                    cout << "Primeiro, registre um usuario.\n";
                 }
-                break;
+            }
+            else
+            {
+                cout << "Primeiro, registre um usuario.\n";
+            }
+            break;
 
-            case 4:
-                // Verificar disponibilidade de um livro
-                cout << "Título do livro para verificar disponibilidade: ";
-                getline(cin, titulo);
-                if (Biblioteca::verificarDisponibilidade(titulo)) {
-                    cout << "O livro está disponível para Emprestimo.\n";
-                } else {
-                    cout << "O livro não está disponível para Emprestimo.\n";
-                }
-                break;
+        case 4:
+            // Verificar disponibilidade de um livro
+            cout << "Título do livro para verificar disponibilidade: ";
+            getline(cin, titulo);
+            if (Biblioteca::verificarDisponibilidade(titulo))
+            {
+                cout << "O livro está disponível para Emprestimo.\n";
+            }
+            else
+            {
+                cout << "O livro não está disponível para Emprestimo.\n";
+            }
+            break;
 
-            case 5:
-                // Listar livros emprestados por um usuario
-                if (usuarioAtual) {
-                    cout << "Livros emprestados para " << usuarioAtual->nome << ":\n";
-                    Biblioteca::listarLivrosEmprestados(*usuarioAtual);
-                } else {
-                    cout << "Primeiro, registre um usuario.\n";
-                }
-                break;
+        case 5:
+            // Listar livros emprestados por um usuario
+            if (usuarioAtual)
+            {
+                cout << "Livros emprestados para " << usuarioAtual->nome << ":\n";
+                Biblioteca::listarLivrosEmprestados(*usuarioAtual);
+            }
+            else
+            {
+                cout << "Primeiro, registre um usuario.\n";
+            }
+            break;
 
-            case 6:
-                // Sair do programa
-                if (usuarioAtual) delete usuarioAtual;
-                return 0;
+        case 6:
+            // Sair do programa
+            if (usuarioAtual)
+                delete usuarioAtual;
+            return 0;
 
-            default:
-                cout << "Opção inválida. Tente novamente.\n";
-                break;
+        default:
+            cout << "Opção inválida. Tente novamente.\n";
+            break;
         }
     }
 }
